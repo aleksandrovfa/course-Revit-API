67e784f402177612e39cb11ac1352bffb8d3f329
Федор Александров <fedfed95@yandex.ru>
2021-12-20 20:37:36 +0300

    Добавьте файлы проекта.

---
+++ 01_Task/01_Part/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Autodesk.Revit.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Part
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Class1
+    {
+    }
+}
